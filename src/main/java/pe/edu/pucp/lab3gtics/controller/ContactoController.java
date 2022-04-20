e4f8bdc97e227727dce496737d4e620a56261ec2
Darwin Aponte <darwin.aponte@pucp.edu.pe>
2022-04-19 22:31:10 -0500

    ajustes de contacto controller y repository

--- src/main/java/pe/edu/pucp/lab3gtics/controller/ContactoController.java
+++ src/main/java/pe/edu/pucp/lab3gtics/controller/ContactoController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pe.edu.pucp.lab3gtics.entity.Cuenta;
 import pe.edu.pucp.lab3gtics.repository.ContactoRepository;
 
 import java.util.Optional;
@@ -20,6 +23,24 @@ public class ContactoController {
     public String ContactoPorMascota(Model model){
         model.addAttribute("listaContacto", contactoRepository.obtenerContactoMascota());
         return "/contacto/lista";
+    }
+
+    @GetMapping("/newContacto")
+    public String nuevoContactoForm() {
+
+        return "/contacto/newContacto";
+    }
+
+    @PostMapping("/save")
+    public String guardarContacto(Cuenta cuenta, RedirectAttributes attr) {
+        if (cuenta.getId()== 0) {
+            attr.addFlashAttribute("msg", "Contacto creado exitosamente");
+        } else {
+            attr.addFlashAttribute("msg", "Contacto actualizado exitosamente");
+        }
+        contactoRepository.save(cuenta);
+        return "redirect:/contacto/listaContacto";
+
     }
 
 
--- src/main/java/pe/edu/pucp/lab3gtics/repository/ContactoRepository.java
+++ src/main/java/pe/edu/pucp/lab3gtics/repository/ContactoRepository.java
@@ -1,14 +1,16 @@
 package pe.edu.pucp.lab3gtics.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.pucp.lab3gtics.dto.ContactoMascota;
+import pe.edu.pucp.lab3gtics.entity.Cuenta;
 
 
 import java.util.List;
 
 @Repository
-public interface ContactoRepository {
+public interface ContactoRepository extends JpaRepository<Cuenta, Integer> {
 
     @Query(value = "select cu.idcuenta as contacto, count(ma.idmascota) as mascotas,  cu.correo, cu.direccion, cu.telefono from cuenta as cu\n" +
             "inner join mascota ma on cu.idcuenta=ma.cuenta_idcuenta\n" +
