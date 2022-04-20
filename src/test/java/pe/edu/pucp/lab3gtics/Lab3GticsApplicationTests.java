bb4bce83694fe20b4a4dafa174d06bb6d0ebfdfd
Rodrigo Loayza <loayza.rs@pucp.edu.pe>
2022-04-19 19:35:02 -0500

    Archivos de config

---
+++ src/test/java/pe/edu/pucp/lab3gtics/Lab3GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.lab3gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
