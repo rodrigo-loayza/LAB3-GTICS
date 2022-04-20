3b74b8b45b742a0eb9d173ddf57522db8ba5dffc
Darwin Aponte <darwin.aponte@pucp.edu.pe>
2022-04-20 13:02:06 -0500

    ver mas contacto listo

--- src/main/java/pe/edu/pucp/lab3gtics/controller/ContactoController.java
+++ src/main/java/pe/edu/pucp/lab3gtics/controller/ContactoController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pe.edu.pucp.lab3gtics.entity.Cuenta;
+import pe.edu.pucp.lab3gtics.entity.Mascota;
 import pe.edu.pucp.lab3gtics.repository.CuentaRepository;
+import pe.edu.pucp.lab3gtics.repository.MascotaRepository;
 
 
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 
 @Controller
@@ -19,6 +22,8 @@ import java.util.Optional;
 public class ContactoController {
     @Autowired
     CuentaRepository cuentaRepository;
+    @Autowired
+    MascotaRepository mascotaRepository;
 
 
     @GetMapping(value = {"", "/","/listaContacto"})
@@ -44,11 +49,40 @@ public class ContactoController {
         return "redirect:/contacto/listaContacto";
     }
 
+
     @GetMapping("/verMas")
     public String verMasInfo(Model model, @RequestParam("id") int id){
-        model.addAttribute("mascotaPorContacto",cuentaRepository.obtenerMascotasPorContacto(id));
-        model.addAttribute("id",id);
-        return "/contacto/vermas";
+        Optional<Cuenta> optionalCuenta = cuentaRepository.findById(id);
+        if (optionalCuenta.isPresent()) {
+            Cuenta cuenta = optionalCuenta.get();
+            model.addAttribute("mascotaPorContacto",cuentaRepository.obtenerMascotasPorContacto(id));
+            model.addAttribute("contacto",cuenta);
+            model.addAttribute("id",id);
+            return "/contacto/vermas";
+        } else {
+            return "redirect:/contacto/listaContacto";
+        }
+
+//        model.addAttribute("mascotaPorContacto",cuentaRepository.obtenerMascotasPorContacto(id));
+//        model.addAttribute("contacto",cuentaRepository.findById(id));
+//        model.addAttribute("id",id);
+//        return "/contacto/vermas";
+    }
+
+    @GetMapping("/delete")
+    public String borrarMascota(Model model,
+                                 @RequestParam("id") int id,
+                                 RedirectAttributes attr) {
+
+        Optional<Mascota> optionalMascota = mascotaRepository.findById(id);
+//        Optional<Employees> optionalEmployees = employeesRepository.findById(id);
+
+        if (optionalMascota.isPresent()) {
+            mascotaRepository.deleteById(id);
+            attr.addFlashAttribute("msg","Mascota borrado exitosamente");
+        }
+        return "redirect:/contacto/listaContacto";
+
     }
 
 
